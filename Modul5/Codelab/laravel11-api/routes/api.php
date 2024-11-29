a72d052fc02b72dab5d3c740d755ca388b15b189
KML_23 <1kemalzth@gmail.com>
2024-11-29 21:55:22 +0700

    Codelab Modul5

---
+++ Modul5/Codelab/laravel11-api/routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Auth\Middleware\Authenticate;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\PostController;
+
+// Route for getting authenticated user
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware(Authenticate::using('sanctum'));
+
+// Route for posts
+Route::apiResource('/posts', PostController::class);
